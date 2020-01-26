2eb00322e147ba5a54b71217296612a2d650ff4a
Brayan Lozano <thebrayanlozano@gmail.com>
2020-01-26 11:50:54 -0500

    Preparing for Deplay

---
+++ public/precache-manifest.b132ad09f1aa637f3af755848111548f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd2ce24f0b8b43e86a7de7f60d71f1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7d4aa3fde439b303373",
+    "url": "/static/css/main.75ea8a95.chunk.css"
+  },
+  {
+    "revision": "23dbaa77a60b4d7774cf",
+    "url": "/static/js/2.d2202ea5.chunk.js"
+  },
+  {
+    "revision": "335932bcfe373407d7c8acc8ad15eddd",
+    "url": "/static/js/2.d2202ea5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7d4aa3fde439b303373",
+    "url": "/static/js/main.d92940da.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "f751c040a2fdbdda3010475ed128f86b",
+    "url": "/static/media/logo.f751c040.png"
+  }
+]);
--- server.js
+++ server.js
@@ -3,14 +3,24 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const cors = require('cors');
 const app = express();
+const path = require('path')
 
 // Allow cross-origin
 app.use(cors());
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-}));
+app.use(
+    '/graphql', 
+    graphqlHTTP({
+        schema,
+        graphiql: true
+    })
+);
+
+app.use(express.static('public'))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 
